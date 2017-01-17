b11c54a5501d72eb9474f0e1565b2e82ca5914f2
Paolo Basso <paolo.basso87@gmail.com>
2017-01-17 14:55:00 +0100

    Initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import BooksReducer form './reducer_books';
+
+const rootReducer = combineReducers({
+  books: BooksReducer
+});
+
+export default rootReducer;
